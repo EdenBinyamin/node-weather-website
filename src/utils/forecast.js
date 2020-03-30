d3a64d0cb4f85765165ff86ff0259a120a765e49
unknown <binyamineden@gmail.com>
2020-03-30 11:12:03 +0300

    Add some more new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/d6c3045314cdb0ad994d6f1125a2f7ea/' + latitude + ',' + longitude + '?units=si'
     request({ url, json: true }, (error, { body }) => {
@@ -8,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         else {
             currently = body.currently
-            callback(undefined, "It is currently " + currently.temperature + " degrees out, There is a " + currently.precipProbability * 100 + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + " It is currently " + currently.temperature + " degrees out, There is a " + currently.precipProbability * 100 + "% chance of rain. The highest tempature today will be " + body.daily.data[0].temperatureHigh + ", and the lowest tempature today will be " + body.daily.data[0].temperatureLow)
         }
     })
 }
